2e175e10535b825e6acc1537da6bf1afb187844e
Zack Angelo <zackangelo@gmail.com>
2025-04-20 13:26:43 -0500

    remove unused hono endpoints in fullstack template

--- templates/template-fullstack-typescript/api/src/index.ts
+++ templates/template-fullstack-typescript/api/src/index.ts
@@ -1,96 +1,11 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { stream, streamSSE } from "hono/streaming";
-import { user, assistant, open } from "@mixlayer/llm";
+import { streamSSE } from "hono/streaming";
+import { assistant, open } from "@mixlayer/llm";
 
 const app = new Hono();
 
 app.use(logger());
-
-const MATH_TOOL = {
-  name: "calculator",
-  description: "Evaluate mathematical expressions",
-  fn: ({ expression }: { expression: string }) => {
-    return { answer: eval(expression) };
-  },
-  parameters: {
-    expression: {
-      param_type: "string",
-      description: "Math problem to evaluate",
-      required: true,
-    },
-  },
-};
-
-app.get("/dru", async (c) => {
-  const seq = await open();
-  await user(seq).append("What is etymology of the name Dru?\n");
-  const out = await assistant(seq)
-    .gen({ temperature: 0.2, stopAt: ["\n"] })
-    .text();
-  return c.text(out);
-});
-
-app.get("/textGen", async (c) => {
-  return stream(c, async (stream) => {
-    const seq = await open();
-    await user(seq).append("What is the meaning of Miguel?\n");
-    await stream.pipe(await assistant(seq).gen().textStream());
-  });
-});
-
-app.get("/api/hello", (c) => {
-  return c.json({
-    ok: true,
-    message: "Hello World!",
-  });
-});
-
-app.get("/hello/:name", (c) => {
-  const { name } = c.req.param();
-  return c.text(`Hello, ${name}!`);
-});
-
-app.get("/boom", (c) => {
-  throw new Error("hello source maps");
-});
-
-app.get("/stream", (c) => {
-  return stream(c, async (stream) => {
-    for (let i = 0; i < 10; i++) {
-      await stream.writeln(`Hello, ${i}!`);
-      await stream.sleep(250);
-    }
-  });
-});
-
-app.get("/stream3", (c) => {
-  //@ts-ignore
-  let stream = test_js_stream();
-  return new Response(stream);
-});
-
-app.get("/stream2", (c) => {
-  let stream = new ReadableStream({
-    start(controller) {
-      console.log("start");
-      controller.enqueue("Hello\n");
-    },
-
-    async pull(controller) {
-      console.log("pull");
-      const encoder = new TextEncoder();
-      controller.enqueue(encoder.encode("<MESSAGE>\n"));
-      controller.close();
-    },
-
-    cancel() {
-      console.log("cancel");
-    },
-  });
-
-  return new Response(stream);
-});
 
 app.post("/chat", async (c) => {
   const body = await c.req.json<ChatRequest>();
